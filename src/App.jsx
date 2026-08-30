8b4afed4bb73783910580652c33e997b6304db83
karthick <karthickmv14@gmail.com>
2026-08-30 21:41:54 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <>
-    <Navbar/>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 
 function Home() {
     return (
-        <div>
+        <>
+            <div>
+                <section className='section-one'>
+                    <div className='container'>
+                        <div className='row'>
+                            <div className='col-lg-6'>
+                                the
+                            </div>
+                            <div className='col-lg-6'>
+                                the
+                            </div>
+                        </div>
+                    </div>
+                </section>
 
-        </div>
+            </div>
+        </>
     )
 }
 
